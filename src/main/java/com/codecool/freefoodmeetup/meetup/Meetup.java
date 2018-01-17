d58e7da405f2178ed64eefee933dacf1a37c93eb
jakubskiba <jakubskibakrk@gmail.com>
2018-01-17 21:47:05 +0100

    implement meetup model

---
+++ src/main/java/com/codecool/freefoodmeetup/meetup/Meetup.java
@@ -0,0 +1,57 @@
+package com.codecool.freefoodmeetup.meetup;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Meetup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private LocalDateTime date;
+
+    @NotEmpty
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
