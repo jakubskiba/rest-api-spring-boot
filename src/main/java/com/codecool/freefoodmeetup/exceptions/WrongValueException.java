c5a8836c231f6941bda8e5a7e9b011f4a38b8897
jakubskiba <jakubskibakrk@gmail.com>
2018-01-17 20:31:05 +0100

    implement wrong value exception

---
+++ src/main/java/com/codecool/freefoodmeetup/exceptions/WrongValueException.java
@@ -0,0 +1,8 @@
+package com.codecool.freefoodmeetup.exceptions;
+
+public class WrongValueException extends RuntimeException {
+    public WrongValueException(String message) {
+        super(message);
+    }
+
+}
