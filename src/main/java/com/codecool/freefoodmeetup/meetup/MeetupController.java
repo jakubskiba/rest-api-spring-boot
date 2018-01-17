8136d6d1c198d9b018afdfa5d2376ba51d908218
jakubskiba <jakubskibakrk@gmail.com>
2018-01-17 21:51:06 +0100

    create meetup controller

---
+++ src/main/java/com/codecool/freefoodmeetup/meetup/MeetupController.java
@@ -0,0 +1,17 @@
+package com.codecool.freefoodmeetup.meetup;
+
+import com.codecool.freefoodmeetup.exceptions.ResourceNotFoundException;
+import com.sun.org.apache.xml.internal.security.utils.resolver.ResourceResolverException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/meetups")
+public class MeetupController {
+    private MeetupServiceInterface service;
+
+    public MeetupController(MeetupServiceInterface service) {
+        this.service = service;
+    }
+
+
+}
