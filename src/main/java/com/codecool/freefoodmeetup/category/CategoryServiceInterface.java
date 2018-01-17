c63424170d6e9fa0f9ab935bb06e326b2d1933b5
jakubskiba <jakubskibakrk@gmail.com>
2018-01-17 21:16:24 +0100

    remove throws from category service

--- src/main/java/com/codecool/freefoodmeetup/category/CategoryServiceInterface.java
+++ src/main/java/com/codecool/freefoodmeetup/category/CategoryServiceInterface.java
@@ -1,12 +1,13 @@
 package com.codecool.freefoodmeetup.category;
 
 import com.codecool.freefoodmeetup.exceptions.ResourceNotFoundException;
+import com.codecool.freefoodmeetup.exceptions.WrongValueException;
 import org.springframework.stereotype.Component;
 
-@Component
 public interface CategoryServiceInterface {
     Iterable<Category> findAll();
-    Category findOne(Integer id) throws ResourceNotFoundException;
-    void save(Category category);
-    void delete(Integer id) throws ResourceNotFoundException;
+    Category findOne(Integer id);
+    Category save(Category category);
+    void delete(Integer id);
+    Category update(Category category);
 }
