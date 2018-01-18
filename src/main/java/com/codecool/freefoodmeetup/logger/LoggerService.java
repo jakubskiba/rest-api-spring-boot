c292b56c3d3bd576c22fba787b4cbd39e6f5d07c
jakubskiba <jakubskibakrk@gmail.com>
2018-01-18 17:57:42 +0100

    configure logger

--- src/main/java/com/codecool/freefoodmeetup/logger/LoggerService.java
+++ src/main/java/com/codecool/freefoodmeetup/logger/LoggerService.java
@@ -3,19 +3,22 @@ package com.codecool.freefoodmeetup.logger;
 import com.codecool.freefoodmeetup.FreeFoodMeetupApplication;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoggerService implements LoggerServiceInterface {
     private static final Logger logger = LogManager.getLogger(FreeFoodMeetupApplication.class);
+    private Marker marker = MarkerManager.getMarker("START");
 
     @Override
     public void logError(String message) {
-        logger.error(message);
+        logger.error(marker, message);
     }
 
     @Override
     public void logInfo(String message) {
-        logger.info(message);
+        logger.info(marker, message);
     }
 }
