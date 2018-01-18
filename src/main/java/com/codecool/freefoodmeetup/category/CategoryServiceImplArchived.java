ef413a1c12ad5c5679d00da7b5435e2f8fafe498
jakubskiba <jakubskibakrk@gmail.com>
2018-01-18 17:02:56 +0100

    apply logger to category resource

--- src/main/java/com/codecool/freefoodmeetup/category/CategoryServiceImplArchived.java
+++ src/main/java/com/codecool/freefoodmeetup/category/CategoryServiceImplArchived.java
@@ -1,68 +1,103 @@
 package com.codecool.freefoodmeetup.category;
 
 import com.codecool.freefoodmeetup.exceptions.ResourceNotFoundException;
+import com.codecool.freefoodmeetup.logger.LoggerService;
 import com.codecool.freefoodmeetup.meetup.Meetup;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import sun.rmi.runtime.Log;
 
 @Primary
 @Service
 public class CategoryServiceImplArchived implements CategoryServiceInterface {
     private CategoryRepository repository;
     private CategoryValidator validator;
+    private LoggerService logger;
 
-    public CategoryServiceImplArchived(CategoryRepository repository, CategoryValidator validator) {
+    public CategoryServiceImplArchived(CategoryRepository repository, CategoryValidator validator, LoggerService logger) {
         this.repository = repository;
         this.validator = validator;
+        this.logger = logger;
     }
 
     public Iterable<Category> findAll() {
+        logger.logInfo("Looking for all categories...");
+        logger.logInfo("All categories found");
+
         return this.repository.findByArchived(false);
     }
 
     public Category findOne(Integer id) {
+        logger.logInfo("Looking for category...");
         checkExistence(id);
         checkArchivisation(id);
+
+        logger.logInfo("Category by id: " + id +" found");
+
         return this.repository.findOne(id);
     }
 
     public Category save(Category category) {
+        logger.logInfo("Creating category...");
+
         this.validator.checkIdIsNull(category);
         this.validator.checkNotEmptyFields(category);
+
+        logger.logInfo("New category created");
+
         return this.repository.save(category);
     }
 
     public Category update(Category category) {
+        logger.logInfo("Updating category...");
+
         Integer id = category.getId();
         this.validator.checkHasId(category);
         checkExistence(id);
         checkArchivisation(id);
 
         this.validator.checkNotEmptyFields(category);
+
+        logger.logInfo("Category updated");
+
         return this.repository.save(category);
     }
 
     @Override
     public void delete(Integer id) {
+        logger.logInfo("Archiving category...");
+
         checkExistence(id);
         checkArchivisation(id);
+
         Category category = this.repository.findOne(id);
+        logger.logInfo("Archiving category of id: " + id);
         for(Meetup meetup : category.getMeetups()) {
             meetup.setArchived(true);
+            logger.logInfo("Archiving meetup of id: " + meetup.getId());
         }
         category.setArchived(true);
+
+        logger.logInfo("Archiving complete");
         this.repository.save(category);
     }
 
     private void checkExistence(Integer id) {
+        logger.logInfo("Looking for Category of id: " + id);
         if(!this.repository.exists(id) ) {
+            logger.logInfo("Looking for Category of id: " + id + " not found!");
             throw new ResourceNotFoundException("No category of id: " + id);
         }
+        logger.logInfo("Looking for Category of id: " + id + " found!");
     }
 
     private void checkArchivisation(Integer id) {
+        logger.logInfo("Checking is Category of id: "+ id + " archived...");
         if(this.repository.findByIdAndArchived(id, true) != null) {
+            logger.logInfo("Category of id: " + id + " is archived");
             throw new ResourceNotFoundException("Category of id: " + id + " was archived");
         }
+        logger.logInfo("Category of id: " + id + " is not archived");
     }
 }
--- src/main/java/com/codecool/freefoodmeetup/category/CategoryValidator.java
+++ src/main/java/com/codecool/freefoodmeetup/category/CategoryValidator.java
@@ -1,27 +1,40 @@
 package com.codecool.freefoodmeetup.category;
 
 import com.codecool.freefoodmeetup.exceptions.WrongValueException;
+import com.codecool.freefoodmeetup.logger.LoggerService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CategoryValidator {
+    private LoggerService logger;
+
+    public CategoryValidator(LoggerService logger) {
+        this.logger = logger;
+    }
+
     public void checkNotEmptyFields(Category category) {
+        logger.logInfo("Checking for category has empty fields");
         if(category.getName() == null) {
+            logger.logError("Category has null name!");
             throw new WrongValueException("Name can not be null");
         } else if(category.getName().isEmpty()) {
+            logger.logError("Category has empty name field!");
             throw new WrongValueException("Name can not be empty");
         }
     }
 
     public void checkIdIsNull(Category category) {
+        logger.logInfo("Checking Category donsn't have id");
         if(category.getId() != null) {
+            logger.logError("Given category have id");
             throw new WrongValueException("Id should not be given");
         }
     }
 
     public void checkHasId(Category category) {
+        logger.logInfo("Checking Category have id");
         if(category.getId() == null) {
-            throw new WrongValueException("Id should be given");
+            throw new WrongValueException("Id should not be given");
         }
     }
 }
